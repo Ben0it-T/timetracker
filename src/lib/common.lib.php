f95890924494d2a7d12b04b03c602b22b25447e5
Ben0it-T <37017213+Ben0it-T@users.noreply.github.com>
2024-10-28 11:10:57 +0100

    Update ttRandomString

--- src/lib/common.lib.php
+++ src/lib/common.lib.php
@@ -648,7 +648,7 @@ function ttRandomString($length = 32) {
   $charactersLength = strlen($characters);
   $randomString = '';
   for ($i = 0; $i < $length; $i++) {
-    $randomString .= $characters[rand(0, $charactersLength - 1)];
+    $randomString .= $characters[random_int(0, $charactersLength - 1)];
   }
   return $randomString;
 }
